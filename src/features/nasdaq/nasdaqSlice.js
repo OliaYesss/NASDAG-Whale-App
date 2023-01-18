74a89660df440fe328aec7e739635eabe9c1f820
Olha Shekhet <oliayesss@gmail.com>
2023-01-18 15:20:54 -0800

    Slice added

--- src/features/nasdaq/nasdaqSlice.js
+++ src/features/nasdaq/nasdaqSlice.js
@@ -13,3 +13,34 @@ const parsePrice = (value) => {
   return parseFloat(value.replace(/,/g, ''))
 }
 
+const nasdaqSlice = createSlice({
+  name: 'nasdaq',
+  initialState,
+
+  reducers: {
+    addNasdaqData: (state, action) => {
+      state.nasdaqData = action.payload.nasdaqData
+    },
+    addNasdaqDataArray: (state, action) => {
+      const date = new Date(action.payload.timestamp)
+      const lastSalePrice = parsePrice(action.payload.lastSalePrice)
+
+      state.nasdaqDataArray.push({
+        y: lastSalePrice,
+        x: date.toLocaleTimeString()
+      })
+    },
+    addCurrNasdaqIndx: (state, action) => {
+      state.currNasdaqIndx = action.payload.currNasdaqIndx
+      state.isHover = action.payload.isHover
+    },
+    addNasdaqLastDataArray: (state, action) => {
+      const data = action.payload.data
+      // const date = action.payload.data.timestamp
+      // const lastSalePrice = action.payload.data.lastSalePrice
+
+      state.nasdaqDataArray.unshift(...data)
+    }
+  }
+})
+
