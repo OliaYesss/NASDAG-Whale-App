ca85a3db95c1ee4c250e32be057eb498b500a0cb
Olha Shekhet <oliayesss@gmail.com>
2023-04-10 15:13:53 -0700

    updates

---
+++ src/features/nasdaq/NasdaqApp.js
@@ -0,0 +1,18 @@
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { VisxGraph } from "../../components/VisxGraph";
+import { currNasdaqIndxSelector, isHoverSelector, nasdaqDataSelector, nasdaqInitialIndexSelector, nasdaqLastIndxSelector } from "./nasdaqSlice";
+import React, { useState } from "react";
+
+
+export const NasdaqApp = () => {
+
+  useEffect(() => {
+  }, [])
+
+  return(
+    <>
+
+    </>
+  )
+}
